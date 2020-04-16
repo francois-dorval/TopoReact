82bf62906431001c6a41ed1730ce0f8b3bcd77ee
c5819 <francois.dorval@gmail.com>
2020-04-16 06:51:26 +0200

    navigation mixte pourrie mais qui marche - drawer dynamique + menu

--- TopoReact/components/SecteurMenu.js
+++ TopoReact/components/SecteurMenu.js
@@ -3,9 +3,11 @@ import React from 'react';
 import {StyleSheet, Dimensions, Image, FlatList} from "react-native";
 import {theme} from "galio-framework";
 import {argonTheme, tabs} from "../constants/";
+import {StyleSheet, Dimensions, Image, FlatList} from "react-native";
 
+import {Card} from "react-native-elements";
 
-import {Button, Card, Select, Icon, Input, Header, Switch} from "../components/";
+import {Button} from "../components/";
 
 const {width} = Dimensions.get("screen");
 
@@ -23,7 +25,7 @@ module.exports = class SecteurMenu extends React.Component {
         const id = this.props.id
         const name = this.props.name
 
-       // console.log("SecteurMenu " + id + " - " + name)
+        // console.log("SecteurMenu " + id + " - " + name)
 
 
         return (
@@ -45,64 +47,64 @@ module.exports = class SecteurMenu extends React.Component {
 
                         title={this.props.name}>{this.props.name}</Button>
 
-                            </View>
-                            );
-                            }
-                            }
+            </View>
+        );
+    }
+}
 
-                            const styles = StyleSheet.create({
-                            title: {
-                            paddingBottom: theme.SIZES.BASE,
-                            paddingHorizontal: theme.SIZES.BASE * 2,
-                            marginTop: 44,
-                            color: argonTheme.COLORS.HEADER
-                            },
-                            group: {
-                            paddingTop: theme.SIZES.BASE * 2
-                            },
-                            shadow: {
-                            shadowColor: "black",
-                            shadowOffset: {width: 0, height: 2},
-                            shadowRadius: 4,
-                            shadowOpacity: 0.2,
-                            elevation: 2
-                            },
-                            button: {
-                            marginBottom: theme.SIZES.BASE,
-                            width: width - theme.SIZES.BASE * 2
-                            },
-                            optionsButton: {
-                            width: "auto",
-                            height: 34,
-                            paddingHorizontal: theme.SIZES.BASE,
-                            paddingVertical: 10
-                            },
-                            input: {
-                            borderBottomWidth: 1
-                            },
-                            inputDefault: {
-                            borderBottomColor: argonTheme.COLORS.PLACEHOLDER
-                            },
-                            inputTheme: {
-                            borderBottomColor: argonTheme.COLORS.PRIMARY
-                            },
-                            inputInfo: {
-                            borderBottomColor: argonTheme.COLORS.INFO
-                            },
-                            inputSuccess: {
-                            borderBottomColor: argonTheme.COLORS.SUCCESS
-                            },
-                            inputWarning: {
-                            borderBottomColor: argonTheme.COLORS.WARNING
-                            },
-                            inputDanger: {
-                            borderBottomColor: argonTheme.COLORS.ERROR
-                            },
-                            social: {
-                            width: theme.SIZES.BASE * 3.5,
-                            height: theme.SIZES.BASE * 3.5,
-                            borderRadius: theme.SIZES.BASE * 1.75,
-                            justifyContent: "center"
-                            },
-                            });
+const styles = StyleSheet.create({
+    title: {
+        paddingBottom: theme.SIZES.BASE,
+        paddingHorizontal: theme.SIZES.BASE * 2,
+        marginTop: 44,
+        color: argonTheme.COLORS.HEADER
+    },
+    group: {
+        paddingTop: theme.SIZES.BASE * 2
+    },
+    shadow: {
+        shadowColor: "black",
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 4,
+        shadowOpacity: 0.2,
+        elevation: 2
+    },
+    button: {
+        marginBottom: theme.SIZES.BASE,
+        width: width - theme.SIZES.BASE * 2
+    },
+    optionsButton: {
+        width: "auto",
+        height: 34,
+        paddingHorizontal: theme.SIZES.BASE,
+        paddingVertical: 10
+    },
+    input: {
+        borderBottomWidth: 1
+    },
+    inputDefault: {
+        borderBottomColor: argonTheme.COLORS.PLACEHOLDER
+    },
+    inputTheme: {
+        borderBottomColor: argonTheme.COLORS.PRIMARY
+    },
+    inputInfo: {
+        borderBottomColor: argonTheme.COLORS.INFO
+    },
+    inputSuccess: {
+        borderBottomColor: argonTheme.COLORS.SUCCESS
+    },
+    inputWarning: {
+        borderBottomColor: argonTheme.COLORS.WARNING
+    },
+    inputDanger: {
+        borderBottomColor: argonTheme.COLORS.ERROR
+    },
+    social: {
+        width: theme.SIZES.BASE * 3.5,
+        height: theme.SIZES.BASE * 3.5,
+        borderRadius: theme.SIZES.BASE * 1.75,
+        justifyContent: "center"
+    },
+});
 
